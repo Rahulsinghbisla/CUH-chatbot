6e41d083d8195a0bdcba96455f60002cd413a6c3
Rahul singh <rahulsinghbisla199@gmail.com>
2026-04-22 14:35:06 +0530

    Adding the subscription Feature

--- components/upgrade-component.tsx
+++ components/upgrade-component.tsx
@@ -1,11 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 export function UpgradeComponent() {
   return (
     <div className="absolute left-1/2 -translate-x-1/2">
       <Button
-        onClick={async () => {}}
+        onClick={async () => {
+          await authClient.checkout({
+            // Polar Product IDs
+            products: ["e651f46d-ac20-4f26-b769-ad088b123df2"],
+            // OR
+            // if "products" in passed in the checkout plugin's config, you may pass the slug
+            slug: "cuhbot",
+          });
+        }}
         variant="default"
         className="rounded-full bg-[#373669] border-[#3e3e4a] text-white hover:bg-[#373669]/60 text-[12px] h-8 px-4 font-medium"
       >
--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,17 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import { schema } from "@/db/schema/auth-schema";
 
+import { polar, checkout, portal, usage, webhooks } from "@polar-sh/better-auth"; 
+import { Polar } from "@polar-sh/sdk"; 
+
+const polarClient = new Polar({ 
+    accessToken: process.env.POLAR_ACCESS_TOKEN, 
+    // Use 'sandbox' if you're using the Polar Sandbox environment
+    // Remember that access tokens, products, etc. are completely separated between environments.
+    // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
+    server: 'sandbox'
+});
+
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
@@ -23,6 +34,26 @@ export const auth = betterAuth({
             prompt: "select_account consent", 
         }
     }, 
+    plugins: [
+        polar({ 
+            client: polarClient, 
+            createCustomerOnSignUp: true, 
+            use: [ 
+                checkout({ 
+                    products: [ 
+                        { 
+                            productId: "ec965f5d-a5b1-4502-9050-67c8185e244e", // ID of Product from Polar Dashboard
+                            slug: "cuhbot" // Custom slug for easy reference in Checkout URL, e.g. /checkout/pro
+                        } 
+                    ], 
+                    successUrl: "/success?checkout_id={CHECKOUT_ID}", 
+                    authenticatedUsersOnly: true
+                }), 
+                portal(), 
+                usage(), 
+            ], 
+        }) 
+    ]
     
 });
 
